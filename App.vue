3b2772297458a861485f7fa4991c7b5f8f784ee5
‪Abubakr Elghazawy‬‏ <abubakr8911@gmail.com>
2021-05-25 16:02:09 +0200

    May 25,2021 learn vue native

---
+++ App.vue
@@ -0,0 +1,50 @@
+<template>
+  <view class="container">
+  <text class="text-login">Login</text>
+ 
+    <text-input
+      class="text-input-container"
+      placeholder="user name"
+      v-model="textContent"
+    />
+    <text-input 
+     class="text-input-container"
+     placeholder="password"
+     />
+    <text class="text-container">{{textContent}}</text>
+</view>
+  </template>
+
+ <script>
+export default {
+  data: function() {
+    return {
+      textContent: ""
+    };
+  }
+};
+</script>
+ <style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-container {
+  color: blue;
+  padding: 2;
+  font-size: 22;
+}
+.text-login {
+  font-size:30;
+  color:gray;
+ justify-content: center;
+}
+.text-input-container {
+  width: 300;
+  height: 60;
+  font-size: 22;
+  border-color: 
+}
+</style>
